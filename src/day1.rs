a6bda6f5dcfa7e6bc28f7febb033a80397853f5f
cian-doyle <cian.doyle@ppb.com>
2022-12-01 15:31:23 +0000

    added part two

--- src/day1.rs
+++ src/day1.rs
@@ -1,7 +1,23 @@
-use std::fs;
+use std::{fs::{self}, io::BufRead};
 
-pub fn get_max_calories(path: &str) -> u32 {
-    let calories = fs::read_to_string(path).expect("Can't read file");
+const PUZZLE_INPUT: &str = "data/calories.txt";
+
+// Part 1
+pub fn get_max_calories() -> u32 {
+    let total_calories_vec: Vec<u32> = get_elf_calorie_counts();
+    total_calories_vec.iter().fold(0, |a, &b| a.max(b))
+   
+}
+
+// Part 2
+pub fn get_top_n_max_calories(n: usize) -> u32 {
+    let mut total_calories_vec: Vec<u32> = get_elf_calorie_counts();
+    total_calories_vec.sort();
+    total_calories_vec.drain(total_calories_vec.len() - n ..).sum()
+}
+
+fn get_elf_calorie_counts() -> Vec<u32> {
+    let calories = fs::read_to_string(PUZZLE_INPUT).expect("Can't read file");
     let elf_vec = calories.split("\n\n").collect::<Vec<&str>>();
 
     let mut total_calories_vec = Vec::<u32>::new();
@@ -11,5 +27,8 @@ pub fn get_max_calories(path: &str) -> u32 {
         total_calories_vec.push(sum);
     }
 
-    total_calories_vec.iter().fold(0, |a, &b| a.max(b))
+    total_calories_vec
 }
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use day1::get_max_calories;
+
+use crate::day1::get_top_n_max_calories;
 mod day1;
 
 fn main() {
-    println!("{}", get_max_calories("data/calories.txt"));
+    println!("{}", get_max_calories());
+    println!("{}", get_top_n_max_calories(3));
 }
