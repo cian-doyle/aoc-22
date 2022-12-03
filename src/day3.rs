6b6aaf8c403ccbdae975240efa951567760eda4f
cian-doyle <cian.doyle@ppb.com>
2022-12-03 16:10:03 +0000

    solved day3 part2

--- src/day3.rs
+++ src/day3.rs
@@ -1,11 +1,25 @@
-use std::{fs, collections::HashSet};
+use std::{fs, collections::{HashSet, HashMap}};
 
 const PUZZLE_INPUT: &str = "data/backpacks.txt";
+const CHUNK_SIZE: usize = 3;
 
 #[derive(Debug)]
 struct Rucksack {
     first_component: String,
     second_component: String
+}
+
+impl Rucksack {
+    fn unique_char_str(&self) -> String { // trim duplicates and concat both components into one string
+        let mut unique_chars = HashSet::<char>::new();
+        self.first_component.chars().for_each(|c| { 
+            unique_chars.insert(c);
+        });
+        self.second_component.chars().for_each(|c| { 
+            unique_chars.insert(c);
+        });
+        unique_chars.into_iter().collect()
+    }
 }
 
 impl TryFrom<(&str, &str)> for Rucksack {
@@ -20,10 +34,8 @@ impl TryFrom<(&str, &str)> for Rucksack {
     }
 }
 
-fn find_duplicate_items(data: String) -> Vec<char> {
-    let rucksack_vec = data.split("\n").map(|line| 
-        Rucksack::try_from(line.split_at(line.len()/2)).unwrap() // todo error handling
-    ).collect::<Vec<Rucksack>>();
+fn find_duplicate_items(data: String) -> Vec<char> { // Find commmon char in both components of a rucksack
+    let rucksack_vec = get_rucksacks(data);
 
     rucksack_vec.iter().map(|rs| { 
         let mut seen_chars = HashSet::<char>::new();
@@ -32,12 +44,12 @@ fn find_duplicate_items(data: String) -> Vec<char> {
         });
         rs.second_component.chars().find(|c|
             seen_chars.contains(c)
-        ).unwrap() // todo error handling, assumes duplicate char is in both components
+        ).unwrap() // todo error handling
 
     }).collect::<Vec<char>>()
 }
 
-fn char_to_priority(c: &char) -> u32 { // using ascii codes
+fn char_to_priority(c: &char) -> u32 { // Converted using ascii codes
     if c.is_lowercase() { 
         *c as u32 - 96 
     }
@@ -46,12 +58,47 @@ fn char_to_priority(c: &char) -> u32 { // using ascii codes
     }
 }
 
+fn get_rucksacks(data: String) -> Vec<Rucksack> {
+    data.split('\n').map(|line| 
+        Rucksack::try_from(line.split_at(line.len()/2)).unwrap() // todo error handling
+    ).collect::<Vec<Rucksack>>()
+}
 
-pub fn get_priority_sum() -> u32 {
+pub fn get_priority_sum_part_one() -> u32 {
     let input = fs::read_to_string(PUZZLE_INPUT).expect("Can't read file");
     let duplicate_items = find_duplicate_items(input);
 
-    //println!("Duplicate chars: {:?}", duplicate_items);
-
     duplicate_items.iter().map(char_to_priority).sum::<u32>()
+}
+
+pub fn get_priority_sum_part_two() -> u32 {
+    let input = fs::read_to_string(PUZZLE_INPUT).expect("Can't read file");
+    let rucksack_vec = get_rucksacks(input);
+
+    let elf_groups = rucksack_vec.chunks(CHUNK_SIZE);
+
+    // Strings for each groups aggregated rucksacks with duplicate chars removed
+    let group_rucksacks = elf_groups.map(|group| {
+        group.iter().map(|rucksack| {
+            rucksack.unique_char_str()
+        }).collect::<String>()
+    }).collect::<Vec<String>>();
+
+    // Search each group rucksack for char with 3 occurences and get priority char
+    let mut group_badge_chars = Vec::<char>::new();
+
+    group_rucksacks.iter().for_each(|aggregate_rucksack| {
+        let mut char_occurences = HashMap::<char, u32>::new();
+        aggregate_rucksack.chars().for_each(|c| {
+            char_occurences.entry(c).and_modify(|counter| *counter += 1).or_insert(1);      
+            if *char_occurences.get(&c).unwrap() == 3 {
+                group_badge_chars.push(c);
+            }
+        });
+    });
+
+    // Get sum of priorities
+    group_badge_chars.iter().map(
+        char_to_priority
+    ).sum::<u32>()
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use day1::get_max_calories;
 use day1::get_top_n_max_calories;
 use day2::get_final_score;
 
-use crate::day3::get_priority_sum;
+use day3::get_priority_sum_part_two;
 
 mod day1;
 mod day2;
@@ -12,10 +12,5 @@ fn main() {
     // println!("{}", get_max_calories());
     // println!("{}", get_top_n_max_calories(3));
     // println!("{:?}", get_final_score());
-    println!("{:?}", get_priority_sum());
-
+    println!("{:?}", get_priority_sum_part_two());
 }
-
-// X = Loss
-// Y = Draw 
-// Z = Win
